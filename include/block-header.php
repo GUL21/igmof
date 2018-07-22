576f1ea99b51acaece5e7823e75c136cad3a8360
GUL21 <vladimir.gulko@ukr.net>
2018-07-22 23:34:15 +0300

    Igmof
    site for geochemistry

---
+++ include/block-header.php
@@ -0,0 +1,6 @@
+<img src="img/logo.jpg" id="logo">
+<h1>Інститут геохімії, мінералогії та рудоутворення ім. М.П. Семененка НАН України</h1>
+<form>
+	<input type="text" name="search">
+	<input type="submit" name="submit" value="Пошук">
+</form>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Інститут геохімії, мінералогії та рудоутворення ім. М.П. Семененка НАН України</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div id="block-body">
+	<header id="head">
+		<div id="nav">
+			<ul>
+				<li class="active"><a class="act" href="">Головна</a></li>
+				<li><a href="">Проекти</a></li>
+				<li><a href="">Контакти</a></li>
+			</ul>
+		</div>
+		<div id="block-header">
+			<?php
+				include("include/block-header.php");
+			?>
+		</div>
+	</header>
+	<div id="block-left">
+		<?php
+			include("include/block-left.php");
+		?>
+	</div>
+	<div id="block-right">
+	</div>
+	<div id="block-content">
+	</div>
+	<footer id="foot">
+	</footer>
+	</div>
+</body>
+</html>
